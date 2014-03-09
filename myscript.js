eaeb1a27d37c59ed10c87b2521f32c8e75e536b8
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-09 15:43:59 +0900

    #01

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var mainCtrl = function($scope, $http) {
+    $scope.tasks = [
+        {"body":"do this 1", "done":false},
+        {"body":"do this 2", "done":false},
+        {"body":"do this 3", "done":true},
+        {"body":"do this 4", "done":false},
+    ];
+}
