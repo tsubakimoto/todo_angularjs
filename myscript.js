db58ef9c067e6ecafd3cdba25404a850a236b215
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-09 22:31:06 +0900

    #04

--- myscript.js
+++ myscript.js
@@ -13,11 +13,17 @@ var mainCtrl = function($scope, $http) {
     
     $scope.getDoneCount = function() {
         var count = 0;
-        
         angular.forEach($scope.tasks, function(task) {
             count += task.done ? 1 : 0;
         });
-        
         return count;
+    }
+    
+    $scope.deleteDone = function() {
+        var oldTasks = $scope.tasks;
+        $scope.tasks = [];
+        angular.forEach(oldTasks, function(task) {
+            if (!task.done) $scope.tasks.push(task);
+        });
     }
 }
